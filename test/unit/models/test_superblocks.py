22c0033816470bde47f425d3586ab5ba8b7856f9
farsider350 <31876349+farsider350@users.noreply.github.com>
2019-05-31 09:13:11 +1000

    Unit Tests

--- test/unit/models/test_superblocks.py
+++ test/unit/models/test_superblocks.py
@@ -30,10 +30,10 @@ def go_list_proposals():
     items = [
         {u'AbsoluteYesCount': 1000,
          u'AbstainCount': 7,
-         u'CollateralHash': u'acb67ec3f3566c9b94a26b70b36c1f74a010a37c0950c22d683cc50da324fdca',
-         u'DataHex': u'5b5b2270726f706f73616c222c207b22656e645f65706f6368223a20323132323532303430302c20226e616d65223a20226465616e2d6d696c6c65722d35343933222c20227061796d656e745f61646472657373223a2022795965384b77796155753559737753596d4233713372797838585455753979375569222c20227061796d656e745f616d6f756e74223a2032352e37352c202273746172745f65706f6368223a20313437343236313038362c202274797065223a20312c202275726c223a2022687474703a2f2f6461736863656e7472616c2e6f72672f6465616e2d6d696c6c65722d35343933227d5d5d',
-         u'DataString': u'[["proposal", {"end_epoch": 2122520400, "name": "dean-miller-5493", "payment_address": "yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui", "payment_amount": 25.75, "start_epoch": 1474261086, "type": 1, "url": "http://genixcentral.org/dean-miller-5493"}]]',
-         u'Hash': u'dfd7d63979c0b62456b63d5fc5306dbec451180adee85876cbf5b28c69d1a86c',
+         u'CollateralHash': u'8a530f21f4ba8df826c9e82899a0d8f6c32edaefdfc7901f899a683e264e038e',
+         u'DataHex': u'5b5b2270726f706f73616c222c7b22656e645f65706f6368223a313535393131393631302c226e616d65223a2247656e69782d7465737431222c227061796d656e745f61646472657373223a22676d5554504e42664539346961345274703755697455554e79374259775365393975222c227061796d656e745f616d6f756e74223a313230302c2273746172745f65706f6368223a313535363335343831302c2274797065223a312c2275726c223a2268747470733a2f2f746573742e74657374227d5d5d',
+         u'DataString': u'[["proposal",{"end_epoch":1559119610,"name":"Genix-test1","payment_address":"gmUTPNBfE94ia4Rtp7UitUUNy7BYwSe99u","payment_amount":1200,"start_epoch":1556354810,"type":1,"url":"https://test.test"}]]',
+         u'Hash': u'8a530f21f4ba8df826c9e82899a0d8f6c32edaefdfc7901f899a683e264e038e',
          u'IsValidReason': u'',
          u'NoCount': 25,
          u'YesCount': 1025,
@@ -46,7 +46,7 @@ def go_list_proposals():
          u'AbstainCount': 29,
          u'CollateralHash': u'3efd23283aa98c2c33f80e4d9ed6f277d195b72547b6491f43280380f6aac810',
          u'DataHex': u'5b5b2270726f706f73616c222c207b22656e645f65706f6368223a20323132323532303430302c20226e616d65223a20226665726e616e64657a2d37363235222c20227061796d656e745f61646472657373223a2022795443363268755234595145506e39414a486a6e517878726548536267416f617456222c20227061796d656e745f616d6f756e74223a2033322e30312c202273746172745f65706f6368223a20313437343236313038362c202274797065223a20312c202275726c223a2022687474703a2f2f6461736863656e7472616c2e6f72672f6665726e616e64657a2d37363235227d5d5d',
-         u'DataString': u'[["proposal", {"end_epoch": 2122520400, "name": "fernandez-7625", "payment_address": "yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV", "payment_amount": 32.01, "start_epoch": 1474261086, "type": 1, "url": "http://genixcentral.org/fernandez-7625"}]]',
+         u'DataString': u'[["proposal", {"end_epoch": 2122520400, "name": "fernandez-7625", "payment_address": "GNEFpDobUEwM7ohmArQVTngKXWAoxBuzaA", "payment_amount": 32.01, "start_epoch": 1474261086, "type": 1, "url": "http://genixcentral.org/fernandez-7625"}]]',
          u'Hash': u'0523445762025b2e01a2cd34f1d10f4816cf26ee1796167e5b029901e5873630',
          u'IsValidReason': u'',
          u'NoCount': 56,
@@ -68,8 +68,8 @@ def go_list_superblocks():
         {u'AbsoluteYesCount': 1,
          u'AbstainCount': 0,
          u'CollateralHash': u'0000000000000000000000000000000000000000000000000000000000000000',
-         u'DataHex': u'5b5b2274726967676572222c207b226576656e745f626c6f636b5f686569676874223a2037323639362c20227061796d656e745f616464726573736573223a2022795965384b77796155753559737753596d42337133727978385854557539793755697c795965384b77796155753559737753596d4233713372797838585455753979375569222c20227061796d656e745f616d6f756e7473223a202232352e37353030303030307c32352e3735303030303030222c202274797065223a20327d5d5d',
-         u'DataString': u'[["trigger", {"event_block_height": 72696, "payment_addresses": "yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui|yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui", "payment_amounts": "25.75000000|25.7575000000", "type": 2}]]',
+         u'DataHex': u'5b5b2274726967676572222c207b226576656e745f626c6f636b5f686569676874223a2037323639362c20227061796d656e745f616464726573736573223a2022476251477a676a7a395764327269757159654439585239714c55647a3967747261507c476251477a676a7a395764327269757159654439585239714c55647a396774726150222c20227061796d656e745f616d6f756e7473223a202232352e37353030303030307c32352e37353735303030303030222c202274797065223a20327d5d5d',
+         u'DataString': u'[["trigger", {"event_block_height": 72696, "payment_addresses": "GbQGzgjz9Wd2riuqYeD9XR9qLUdz9gtraP|GbQGzgjz9Wd2riuqYeD9XR9qLUdz9gtraP", "payment_amounts": "25.75000000|25.7575000000", "type": 2}]]',
          u'Hash': u'667c4a53eb81ba14d02860fdb4779e830eb8e98306f9145f3789d347cbeb0721',
          u'IsValidReason': u'',
          u'NoCount': 0,
@@ -82,8 +82,8 @@ def go_list_superblocks():
         {u'AbsoluteYesCount': 1,
          u'AbstainCount': 0,
          u'CollateralHash': u'0000000000000000000000000000000000000000000000000000000000000000',
-         u'DataHex': u'5b5b2274726967676572222c207b226576656e745f626c6f636b5f686569676874223a2037323639362c20227061796d656e745f616464726573736573223a2022795965384b77796155753559737753596d42337133727978385854557539793755697c795965384b77796155753559737753596d4233713372797838585455753979375569222c20227061796d656e745f616d6f756e7473223a202232352e37353030303030307c32352e3735303030303030222c202274797065223a20327d5d5d',
-         u'DataString': u'[["trigger", {"event_block_height": 72696, "payment_addresses": "yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui|yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui", "payment_amounts": "25.75000000|25.75000000", "type": 2}]]',
+         u'DataHex': u'5b5b2274726967676572222c207b226576656e745f626c6f636b5f686569676874223a2037323639362c20227061796d656e745f616464726573736573223a2022476251477a676a7a395764327269757159654439585239714c55647a3967747261507c476251477a676a7a395764327269757159654439585239714c55647a396774726150222c20227061796d656e745f616d6f756e7473223a202232352e37353030303030307c32352e37353735303030303030222c202274797065223a20327d5d5d',
+         u'DataString': u'[["trigger", {"event_block_height": 72696, "payment_addresses": "GbQGzgjz9Wd2riuqYeD9XR9qLUdz9gtraP|GbQGzgjz9Wd2riuqYeD9XR9qLUdz9gtraP", "payment_amounts": "25.75000000|25.75000000", "type": 2}]]',
          u'Hash': u'8f91ffb105739ec7d5b6c0b12000210fcfcc0837d3bb8ca6333ba93ab5fc0bdf',
          u'IsValidReason': u'',
          u'NoCount': 0,
@@ -96,8 +96,8 @@ def go_list_superblocks():
         {u'AbsoluteYesCount': 1,
          u'AbstainCount': 0,
          u'CollateralHash': u'0000000000000000000000000000000000000000000000000000000000000000',
-         u'DataHex': u'5b5b2274726967676572222c207b226576656e745f626c6f636b5f686569676874223a2037323639362c20227061796d656e745f616464726573736573223a2022795965384b77796155753559737753596d42337133727978385854557539793755697c795965384b77796155753559737753596d4233713372797838585455753979375569222c20227061796d656e745f616d6f756e7473223a202232352e37353030303030307c32352e3735303030303030222c202274797065223a20327d5d5d',
-         u'DataString': u'[["trigger", {"event_block_height": 72696, "payment_addresses": "yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui|yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui", "payment_amounts": "25.75000000|25.75000000", "type": 2}]]',
+         u'DataHex': u'5b5b2274726967676572222c207b226576656e745f626c6f636b5f686569676874223a2037323639362c20227061796d656e745f616464726573736573223a2022476251477a676a7a395764327269757159654439585239714c55647a3967747261507c476251477a676a7a395764327269757159654439585239714c55647a396774726150222c20227061796d656e745f616d6f756e7473223a202232352e37353030303030307c32352e37353735303030303030222c202274797065223a20327d5d5d',
+         u'DataString': u'[["trigger", {"event_block_height": 72696, "payment_addresses": "GbQGzgjz9Wd2riuqYeD9XR9qLUdz9gtraP|GbQGzgjz9Wd2riuqYeD9XR9qLUdz9gtraP", "payment_amounts": "25.75000000|25.75000000", "type": 2}]]',
          u'Hash': u'bc2834f357da7504138566727c838e6ada74d079e63b6104701f4f8eb05dae36',
          u'IsValidReason': u'',
          u'NoCount': 0,
@@ -116,7 +116,7 @@ def go_list_superblocks():
 def superblock():
     sb = Superblock(
         event_block_height=62500,
-        payment_addresses='yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui|yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV',
+        payment_addresses='GbQGzgjz9Wd2riuqYeD9XR9qLUdz9gtraP|GNEFpDobUEwM7ohmArQVTngKXWAoxBuzaA',
         payment_amounts='5|3',
         proposal_hashes='e8a0057914a2e1964ae8a945c4723491caae2077a90a00a2aabee22b40081a87|d1ce73527d7cd6f2218f8ca893990bc7d5c6b9334791ce7973bfa22f155f826e',
     )
@@ -156,40 +156,40 @@ def test_superblock_is_valid(superblock):
     assert superblock.is_valid() is True
 
     # mess with payment addresses
-    superblock.payment_addresses = 'yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV|1234 Anywhere ST, Chicago, USA'
+    superblock.payment_addresses = 'GNEFpDobUEwM7ohmArQVTngKXWAoxBuzaA|1234 Anywhere ST, Chicago, USA'
     assert superblock.is_valid() is False
 
     # leading spaces in payment addresses
-    superblock.payment_addresses = ' yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV'
+    superblock.payment_addresses = ' GNEFpDobUEwM7ohmArQVTngKXWAoxBuzaA'
     superblock.payment_amounts = '5.00'
     assert superblock.is_valid() is False
 
     # trailing spaces in payment addresses
-    superblock.payment_addresses = 'yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV '
+    superblock.payment_addresses = 'GNEFpDobUEwM7ohmArQVTngKXWAoxBuzaA '
     superblock.payment_amounts = '5.00'
     assert superblock.is_valid() is False
 
     # leading & trailing spaces in payment addresses
-    superblock.payment_addresses = ' yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV '
+    superblock.payment_addresses = ' GNEFpDobUEwM7ohmArQVTngKXWAoxBuzaA '
     superblock.payment_amounts = '5.00'
     assert superblock.is_valid() is False
 
     # single payment addr/amt is ok
-    superblock.payment_addresses = 'yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV'
+    superblock.payment_addresses = 'GNEFpDobUEwM7ohmArQVTngKXWAoxBuzaA'
     superblock.payment_amounts = '5.00'
     assert superblock.is_valid() is True
 
     # ensure number of payment addresses matches number of payments
-    superblock.payment_addresses = 'yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV'
+    superblock.payment_addresses = 'GNEFpDobUEwM7ohmArQVTngKXWAoxBuzaA'
     superblock.payment_amounts = '37.00|23.24'
     assert superblock.is_valid() is False
 
-    superblock.payment_addresses = 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui|yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV'
+    superblock.payment_addresses = 'GbQGzgjz9Wd2riuqYeD9XR9qLUdz9gtraP|GNEFpDobUEwM7ohmArQVTngKXWAoxBuzaA'
     superblock.payment_amounts = '37.00'
     assert superblock.is_valid() is False
 
     # ensure amounts greater than zero
-    superblock.payment_addresses = 'yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV'
+    superblock.payment_addresses = 'GNEFpDobUEwM7ohmArQVTngKXWAoxBuzaA'
     superblock.payment_amounts = '-37.00'
     assert superblock.is_valid() is False
 
@@ -239,7 +239,7 @@ def test_deterministic_superblock_creation(go_list_proposals):
     sb = genixlib.create_superblock(prop_list, 72000, max_budget, misc.now())
 
     assert sb.event_block_height == 72000
-    assert sb.payment_addresses == 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui|yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV'
+    assert sb.payment_addresses == 'GbQGzgjz9Wd2riuqYeD9XR9qLUdz9gtraP|GNEFpDobUEwM7ohmArQVTngKXWAoxBuzaA'
     assert sb.payment_amounts == '25000.75000000|32000.01000000'
     assert sb.proposal_hashes == 'dfd7d63979c0b62456b63d5fc5306dbec451180adee85876cbf5b28c69d1a86c|0523445762025b2e01a2cd34f1d10f4816cf26ee1796167e5b029901e5873630'
 
